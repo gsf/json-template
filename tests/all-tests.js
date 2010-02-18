2811a2f5eba8a5da4866abfeaa527ee91bf9997d
gsf <gsf@manheim.library.drexel.edu>
2010-02-18 13:33:39 -0500

    Removed old test files and added node-test
    I've removed testSimple.js and testSubstitutions.js, which
    have been renamed to test-simple.js and test-substitutions.js.
    I've also added node-test.js as a stand-in for a someday
    CommonJS-compliant test.js in Node.  For now, however, both
    `narwhal tests/all-tests.js` and `node tests/all-tests.js`
    work (the second requires node-test.js when `require("test")`
    fails).

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -1,5 +1,16 @@
-exports.testSimple = require("./testSimple");
-exports.testSubstitutions = require("./testSubstitutions");
-if (require.main === module.id)
-    require("os").exit(require("test/runner").run(exports));
+exports["test-simple"] = require("./test-simple");
+exports["test-substitutions"] = require("./test-substitutions");
+
+if (module === require.main) {
+  try {
+    require("test").run(exports);
+  } catch (err) {
+    if (err.message === "Cannot find module 'test'") {
+      // Assume node if "test" not found
+      require("./node-test").run(exports);
+    } else {
+      throw err
+    }
+  }
+}
 
--- tests/test.js
+++
@@ -1,7 +0,0 @@
-exports["test-simple"] = require("./test-simple");
-exports["test-substitutions"] = require("./test-substitutions");
-
-if (module === require.main) {
-    require("test").run(exports);
-}
-
